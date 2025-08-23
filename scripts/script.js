87cf6890b1eeb20a7c412d46f9891ccc1fb84a08
ammarali5 <ammaralighazi5@gmail.com>
2025-08-23 17:53:03 +0300

    offers scroll bottons

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,7 @@ window.addEventListener("scroll", () => {
             current = section.getAttribute("id");
         }
     });
-    
+
     navLinks.forEach(link => {
         link.classList.remove("active");
         if (link.getAttribute("href") === "#" + current) {
@@ -21,7 +21,48 @@ window.addEventListener("scroll", () => {
 
 
 
-// nav links scroll buttons
+/* ============================ */
+const offerItems = document.querySelector('.offer-items');
+const offerBtnLeft = document.querySelector('.offer-btn.left');
+const offerBtnRight = document.querySelector('.offer-btn.right');
+
+/* function updateOfferButtons() {
+    const scrollLeft = offerItems.scrollLeft;
+    const maxScroll = offerItems.scrollWidth - offerItems.clientWidth;
+
+    // hide right when at end
+    if (scrollLeft <= 2) {
+        offerBtnRight.style.visibility = "hidden";
+    } else {
+        offerBtnRight.style.visibility = "visible";
+    }
+
+    // hide left when at start
+    if (scrollLeft >= maxScroll - 2) {
+        offerBtnLeft.style.visibility = "hidden";
+    } else {
+        offerBtnLeft.style.visibility = "visible";
+    }
+}
+
+// Scroll events
+offerItems.addEventListener('scroll', updateOfferButtons);*/
+
+ // Button click events
+offerBtnLeft.addEventListener('click', () => {
+    offerItems.scrollBy({ left: -350, behavior: 'smooth' });
+});
+offerBtnRight.addEventListener('click', () => {
+    offerItems.scrollBy({ left: 350, behavior: 'smooth' });
+}); 
+
+/*// Initial check
+window.addEventListener('load', updateOfferButtons);
+window.addEventListener('resize', updateOfferButtons); */
+
+
+
+
 const nav = document.querySelector('.menu-nav');
 const btnLeft = document.querySelector('.scroll-btn.left');
 const btnRight = document.querySelector('.scroll-btn.right');
@@ -29,13 +70,13 @@ const btnRight = document.querySelector('.scroll-btn.right');
 function updateButtons() {
     const scrollLeft = nav.scrollLeft;
     const maxScroll = nav.scrollWidth - nav.clientWidth;
-    
+
     if (Math.abs(scrollLeft) <= 2) {
         btnRight.style.visibility = "hidden";
     } else {
         btnRight.style.visibility = "visible";
     }
-    
+
     if (Math.abs(scrollLeft) >= maxScroll - 2) {
         btnLeft.style.visibility = "hidden";
     } else {
